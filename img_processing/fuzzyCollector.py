f16763516bafb06e7d242d8c81abcf6a362f4d8b
jn202871 <jn202871@gmail.com>
2023-11-12 23:38:01 -0500

    renaming/shuffling files

--- img_processing/fuzzyCollector.py
+++ img_processing/fuzzyCollector.py
@@ -40,16 +40,71 @@ def AI_loop():
   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
   ]
   
-  x_cord = ai.selfX() % 35
-  y_cord = ai.selfY() % 35
-  print(x_cord, " : ", y_cord)
-  #area[x_cord][y_cord] = 3
-  '''
-  for row in area:
-    for val in row:
-      print(val, end="")
-    print()
-  '''
+  headingArr = [
+  ["a","b","c","d","e","f","g","h","i","j","k","l"],
+  ["A","B","C","D","E","F","G","H","I","J","K","L"]
+  ]
+  enemyHeadingArr = [
+  ["o","p","q","r","s","t","u","v","w","x","y","z"],
+  ["O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  ]
+  
+  # Ship Heading And Position Tracking
+  x_cord = int(ai.selfX() / 35)
+  y_cord = 31 - int(ai.selfY() / 35)
+  heading = ai.selfHeadingDeg()
+  if (heading == 360):
+    heading -= 1
+  tracking = ai.selfTrackingDeg()
+  trackDiff = abs(((tracking-heading+540)%360)-180)
+  headingIndex = int(heading/30)
+  trackDiffIndex = False
+  if (trackDiff > 90):
+    trackDiffIndex = True
+  headingSerRow = 0
+  if (trackDiffIndex == True):
+    headingSerRow = 1
+  headingSerCol = headingIndex
+  shipVal = headingArr[headingSerRow][headingSerCol]
+  area[y_cord][x_cord] = shipVal
+  
+  # Enemy Heading And Position Tracking
+  enemy = ai.closestShipId()
+  if (enemy != -1):
+    x_cord = int(ai.screenEnemyX(0) / 35)
+    y_cord = 31 - int(ai.screenEnemyY(0) / 35)
+    heading = ai.enemyHeadingDeg(0)
+    if (heading == 360):
+      heading -= 1
+    tracking = ai.enemyTrackingDeg(0)
+    trackDiff = abs(((tracking-heading+540)%360)-180)
+    headingIndex = int(heading/30)
+    trackDiffIndex = False
+    if (trackDiff > 90):
+      trackDiffIndex = True
+    headingSerRow = 0
+    if (trackDiffIndex == True):
+      headingSerRow = 1
+    headingSerCol = headingIndex
+    enemyVal = enemyHeadingArr[headingSerRow][headingSerCol]
+    area[y_cord][x_cord] = enemyVal
+  
+  # Bullet Tracking
+  bulletIndex = 0
+  bullets = []
+  while(ai.shotAlert(bulletIndex) != -1):
+    bulletX = int(ai.shotX(bulletIndex)/35)
+    bulletY = 31 - int(ai.shotY(bulletIndex)/35)
+    bullets.append([bulletX,bulletY])
+    print(bullets)
+    bulletIndex += 1
+  for bullet in bullets:
+    area[bullet[1]][bullet[0]] = 3
+  if (ai.selfAlive() == 1):
+    for row in area:
+      for val in row:
+        print(val, end="")
+      print()
   #Release keys
   ai.thrust(0)
   ai.turnLeft(0)
