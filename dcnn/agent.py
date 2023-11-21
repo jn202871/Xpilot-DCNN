02774772b301802edb26e62038a3739c653feedd
Annika Hoag <annikajhoag@gmail.com>
2023-11-21 16:58:15 -0500

    Working on agent.py

--- dcnn/agent.py
+++ dcnn/agent.py
@@ -1,8 +1,11 @@
-# Jay Nash & Russell Kosovesky & Annika Hoag 2023 COM407
+# Jay Nash & Russell Kosovsky & Annika Hoag 2023 COM407
 # agent that reads the neural network from the .pt file to control the ship
 
 import libpyAI as ai
+import torch.nn as nn
 import torch
+
+outputs = 0
 
 def AI_loop():
 
@@ -43,44 +46,19 @@ def AI_loop():
     ]
   
 
-  headingArr = [
-  ["a","b","c","d","e","f","g","h","i","j","k","l"],
-  ["A","B","C","D","E","F","G","H","I","J","K","L"]
-  ]
-  enemyHeadingArr = [
-  ["o","p","q","r","s","t","u","v","w","x","y","z"],
-  ["O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-  ]
+    headingArr = [
+    ["a","b","c","d","e","f","g","h","i","j","k","l"],
+    ["A","B","C","D","E","F","G","H","I","J","K","L"]
+    ]
+    enemyHeadingArr = [
+    ["o","p","q","r","s","t","u","v","w","x","y","z"],
+    ["O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    ]
   
-  # Ship Heading And Position Tracking
-  x_cord = int(ai.selfX() / 35)
-  y_cord = 31 - int(ai.selfY() / 35)
-  heading = ai.selfHeadingDeg()
-  if (heading == 360):
-    heading -= 1
-  tracking = ai.selfTrackingDeg()
-  trackDiff = abs(((tracking-heading+540)%360)-180)
-  headingIndex = int(heading/30)
-  trackDiffIndex = False
-  if (trackDiff > 90):
-    trackDiffIndex = True
-  headingSerRow = 0
-  if (trackDiffIndex == True):
-    headingSerRow = 1
-  headingSerCol = headingIndex
-  shipVal = headingArr[headingSerRow][headingSerCol]
-  area[y_cord][x_cord] = 3
-  area[y_cord][x_cord+1] = int(ai.selfSpeed())
-  area[y_cord][x_cord-1] = int(ai.aimdir(0))
-  area[y_cord+1][x_cord] = int(ai.selfHeadingDeg())
-  area[y_cord-1][x_cord] = int(ai.selfTrackingDeg())
-  
-  # Enemy Heading And Position Tracking
-  enemy = ai.closestShipId()
-  if (enemy != -1):
-    ex_cord = int(ai.screenEnemyX(0) / 35)
-    ey_cord = 31 - int(ai.screenEnemyY(0) / 35)
-    heading = ai.enemyHeadingDeg(0)
+    # Ship Heading And Position Tracking
+    x_cord = int(ai.selfX() / 35)
+    y_cord = 31 - int(ai.selfY() / 35)
+    heading = ai.selfHeadingDeg()
     if (heading == 360):
         heading -= 1
     tracking = ai.selfTrackingDeg()
@@ -93,41 +71,101 @@ def AI_loop():
     if (trackDiffIndex == True):
         headingSerRow = 1
     headingSerCol = headingIndex
-    enemyVal = enemyHeadingArr[headingSerRow][headingSerCol]
-    area[ey_cord][ex_cord] = 4
-    area[ey_cord][ex_cord+1] = int(ai.enemySpeed(0))
-    area[y_cord+1][x_cord] = int(ai.enemyHeadingDeg(0))
-    area[y_cord-1][x_cord] = int(ai.enemyTrackingDeg(0))
+    shipVal = headingArr[headingSerRow][headingSerCol]
+    area[y_cord][x_cord] = 3
+    area[y_cord][x_cord+1] = int(ai.selfSpeed())
+    area[y_cord][x_cord-1] = int(ai.aimdir(0))
+    area[y_cord+1][x_cord] = int(ai.selfHeadingDeg())
+    area[y_cord-1][x_cord] = int(ai.selfTrackingDeg())
   
-    # Bullet Tracking
-    bulletIndex = 0
-    bullets = []
-    while(ai.shotAlert(bulletIndex) != -1):
-      bulletX = int(ai.shotX(bulletIndex)/35)
-      bulletY = 31 - int(ai.shotY(bulletIndex)/35)
-      bulletAlert = int(ai.shotAlert(bulletIndex))
-      if (bulletAlert == 30000):
-        bulletAlert = 0
-      bullets.append([bulletX,bulletY,bulletAlert])
-      bulletIndex += 1
-    for bullet in bullets:
-      area[bullet[1]][bullet[0]] = 5
-      area[bullet[1]][bullet[0]+1] = bullet[2]
+    # Enemy Heading And Position Tracking
+    enemy = ai.closestShipId()
+    #print("Enemy: ", enemy)
+    if (enemy == -1): 
+        ex_cord = int(ai.screenEnemyX(0) / 35)
+        ey_cord = 31 - int(ai.screenEnemyY(0) / 35)
+        heading = ai.enemyHeadingDeg(0)
+        if (heading == 360):
+            heading -= 1
+        tracking = ai.selfTrackingDeg()
+        trackDiff = abs(((tracking-heading+540)%360)-180)
+        headingIndex = int(heading/30)
+        trackDiffIndex = False
+        if (trackDiff > 90):
+            trackDiffIndex = True
+        headingSerRow = 0
+        if (trackDiffIndex == True):
+            headingSerRow = 1
+        headingSerCol = headingIndex
+        enemyVal = enemyHeadingArr[headingSerRow][headingSerCol]
+        area[ey_cord][ex_cord] = 4
+        area[ey_cord][ex_cord+1] = int(ai.enemySpeed(0))
+        area[y_cord+1][x_cord] = int(ai.enemyHeadingDeg(0))
+        area[y_cord-1][x_cord] = int(ai.enemyTrackingDeg(0))
+  
+        # Bullet Tracking
+        bulletIndex = 0
+        bullets = []
+        while(ai.shotAlert(bulletIndex) != -1):
+            bulletX = int(ai.shotX(bulletIndex)/35)
+            bulletY = 31 - int(ai.shotY(bulletIndex)/35)
+            bulletAlert = int(ai.shotAlert(bulletIndex))
+            if (bulletAlert == 30000):
+                bulletAlert = 0
+            bullets.append([bulletX,bulletY,bulletAlert])
+            bulletIndex += 1
+        for bullet in bullets:
+            area[bullet[1]][bullet[0]] = 5
+            area[bullet[1]][bullet[0]+1] = bullet[2]
       
-    #End of Get Frame
+        #End of Get Frame
     
-    #Loading Model
-    model = DCNNClassifier()
-    model.load_state_dict(torch.load("model.pt")
-    model.eval()
+        layerwidth = 1024
+        # Construct DCNN classifier
+        class DCNNClassifier(nn.Module):
+            def __init__(self):
+                super(DCNNClassifier, self).__init__()
+                self.conv1 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=3, padding=1)
+                self.fc1 = nn.Linear(128*32*32,layerwidth)
+                self.fc2 = nn.Linear(layerwidth,layerwidth)
+                self.fc3 = nn.Linear(layerwidth,layerwidth)
+                self.fc4 = nn.Linear(layerwidth,4)
+                self.sigmoid = nn.Sigmoid()
+            
+            def forward(self, x):
+                x = self.conv1(x)
+                x = torch.relu(x)
+                x = x.view(x.size(0), -1)
+                x = self.fc1(x)
+                x = torch.relu(x)
+                x = self.fc2(x)
+                x = torch.relu(x)
+                x = self.fc3(x)
+                x = torch.relu(x)
+                x = self.fc4(x)
+                #x = self.sigmoid(x)
+                #x = torch.squeeze(x)
+                return x
     
-    outputs = model(area)
-    #End of Loading Model
+        #Loading Model
+        print("I'm about to go into DCNNClassifier")
+        model = DCNNClassifier()
+        model.load_state_dict(torch.load('model.pt', map_location=torch.device('cpu')))
+        model.eval()
+        print("I'm out of DCNNClassifier")
+        
+        outputs = model(area)
+        #End of Loading Model
+        
+        #Printing outputs
+        #torch.set_printoptions(precision=2)
+        torch.tensor(outputs)
+        
+        #Release keys
+        ai.thrust(0)
+        ai.turnLeft(0)
+        ai.turnRight(0)
+        ai.setPower(25)
     
-    #Release keys
-    ai.thrust(0)
-    ai.turnLeft(0)
-    ai.turnRight(0)
-    ai.setPower(25)
 
 ai.start(AI_loop,["-name","dcnn-agent","-join","localhost"])
