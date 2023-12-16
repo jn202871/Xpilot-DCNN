cc46d615b84501f348362fb3f9246eb84237beaa
jn202871 <jn202871@gmail.com>
2023-12-16 15:42:55 -0500

    modified scripts

--- simple_FuzzyDCNN.sh
+++ simple_FuzzyDCNN.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./xpilots -map .maps/simpleEdit.xp -switchBase 1 -maxRoundTime 30 >/dev/null 2>&1 &
+./xpilots -map .maps/simpleEdit.xp -switchBase 1 -maxRoundTime 30 -roundsToPlay 10 >/dev/null 2>&1 &
 python3 ./bots/baseline.py >/dev/null 2>&1 &
 python3 ./dcnn/simpleAgent.py
